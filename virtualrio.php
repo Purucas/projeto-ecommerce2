bc77442272dfe3475bf3aacf855f3838dcf23855
Roberto Accacio <purucabolinho@hotmail.com>
2018-05-07 19:32:47 -0300

    primeiro comit

---
+++ virtualrio.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>Virtual RIO</title>
+		<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	</head>
+	<body>
+
+		<div class="container-fluid ">
+			<header style="background-color: #32CD32 " >
+				<h1 class ="titulo">Virtual RIO</h1>
+			</header>
+
+			<div class="container-fluid">
+
+  <div class="row" style="background-color: #32CD32">
+
+    <div class="col-md-4">
+      <a href="index.php"><img src="imagens/icone2.jpg" style="width: 50%; height: auto"></a>
+    </div>
+
+  <div class="col-md-4" style="margin-top: 60px; width: 50%">
+      <form class="form-inline text-center" >
+        <input class="form-control" type="search" placeholder="O que você procura? " aria-label="Search" style="width: 100%">
+      </form>
+  </div>
+
+
+  <div class="col-md-4" style="margin-top: 60px; text-align: center">
+    <h5>
+    <a style="color: yellow" href="">Cadastre-se</br></a>
+    <a style="color: yellow" href="">Login</a>
+    </h5>
+  </div>
+
+</div>
+<div class="row">
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark col-md-12">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">HOME<span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">LIVROS <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">CELULARES E TELEFONES <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">CONSOLES E GAMES <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">ELETRODOMÉSTICOS <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">INFORMÁTICA <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">MÓVEIS E DECORAÇÕES <span class="sr-only">(current)</span></a>
+      </li>
+  
+    </ul>
+      </div>
+
+
+</nav>
+
+    
+
+</div>
+			<section>
+				
+								
+				<figure>
+
+					
+				</figure>
+				
+				
+
+			</section>
+		</div>
+		<div class="row">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="imagens/xbox.jpg" alt="First slide">
+      <h3>Promoção</h3>
+      <p>Camisetas em todos os tamanhos (PP ao XXL) </p>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="imagens/tvs1.jpg" alt="Second slide">
+      <h3>Promoção</h3>
+      <p>Grandes Marcas estao em Destaque hoje </p>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="imagens/figure1.jpg" alt="Third slide">
+      <h3>Promoção</h3>
+      <p>Camisetas em todos os tamanhos </p>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="imagens/figure2.jpg" alt="Third slide">
+      <h3>Promoção</h3>
+      <p>Vários livros de grande autores </p>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="imagens/figure3.jpg" alt="Third slide">
+      <h3>Promoção</h3>
+      <p>Grandes lançamentos de Games  </p>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Proximo</span>
+  </a>
+</div>
+</div>
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</body>
+  <section>
+    <div class="row">
+      <figure class="col-md-2">
+      <img src="imagens/figure3.jpg">
+      </figure>
+      <figure class="col-md-2">
+      <img src="imagens/figure3.jpg">
+      </figure>
+      <figure class="col-md-2">
+      <img src="imagens/figure3.jpg">
+      </figure>
+
+    </div>
+  </section>
+  <footer>
+    
+  </footer>
+
+</html>
